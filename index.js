247d3bf6e2d20bdf201e2adc9f8b26a5176d83f3
Trish Wynne <trishas_one@hotmail.com>
2018-08-01 12:43:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var name = "Susan"
+var name = "Joe"
 var height = 70
 // Don't worry about this
 module.exports = { name, height
@@ -6,4 +6,6 @@ module.exports = { name, height
 if (name ==="Susan")
 {
   console.log("The name is correct")
+} else {
+  console.log("Expected the name to be Susan")
 }
